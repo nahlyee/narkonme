41001203bdc1a89fa0b670e0fb73006025d1cae3
Nahlyee Van Brunt <nahlyee@gmail.com>
2013-04-15 21:40:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Narkonme::Application.config.secret_token = '0ecbd364a9754856328aca756348f8d92e39860abe5a4fea84be1b7645ef6b3d183898ac95bf9a68d9a0241d798e6e438f4755d50d454f2540be7297a2c0b133'
